efcd02a15027ed5eaffb2a7705398568f7cca347
Nikhil Agrawal <niagrawal@microsoft.com>
2021-06-05 10:51:29 +0530

    express-sesssion and mongo local setup

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+//MongoDB session store for Express
+const MongoStore = require('connect-mongo');
 
 const app = express();
 
 
+const dbString = "mongodb://localhost:27017/student";
+const dbOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+// connection to mongodb
+const connection = mongoose.connect(dbString,dbOptions,(err)=>{
+    if(err)console.log(err);
+    else console.log("database is connected");
+});
+
+// connection to mongodb only for session data 
+const sessionStore = MongoStore.create({
+    mongoUrl: dbString,
+    mongoOptions: dbOptions,
+    collectionName: 'sessions'
+});
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie:{
+        maxAge: 1000*60*60*24 //1 day
+    } 
+}))
+
+
+app.get('/', (req, res, next) =>{
+    if(req.session.viewCount){
+        req.session.viewCount++;
+    }else{
+        req.session.viewCount=1;
+    }
+    res.send(`<h1>Hello World ${req.session.viewCount}</h1>`)
+})
+
 const middleware1 = (req, res, next) => {
     console.log("I am middleware #1");
     req.demoNumber = 1;
-    const errorObj = new Error('I am a test error');
-    next(errorObj);
+    const errorObj = new Error('I am a test  error');
+    next();
 }
 
 function errorHandler(err, req, res, next){
@@ -19,9 +61,5 @@ function errorHandler(err, req, res, next){
 
 app.use(middleware1);
 app.use(errorHandler);
-
-app.get('/', middleware1, (req, res, next) =>{
-    res.send(`<h1>Hello World ${req.demoNumber}</h1>`)
-})
 
 app.listen(3000);
